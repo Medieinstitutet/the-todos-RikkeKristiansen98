7f56e48f6ab212b112423f510a50113b8eaf3c20
RikkeKristiansen98 <rikke.kristiansen@hotmail.com>
2024-01-26 19:17:25 +0100

    Added input and buttons

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,9 @@ function App() {
 
   return (
     <>
-   <TodoList />
+    <div className='app'>
+      <TodoList />
+      </div>
     </>
   )
 }
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,6 +1,24 @@
+
 export const TodoList = () => {
-    return (
+ 
+  return (
     <>
+    <div className="main-container">
+    <h1><strong>To do list</strong></h1>
+      <ul>
+     <input type="text" />
+   <button className="saveBtn">Save</button>
+              <>     
+    <button className="editBtn">
+                  Edit
+      </button>
+     <button className="deleteBtn">Delete</button>
+              </>
+      </ul>
+      <input type="text"
+        placeholder="Add task to do" />
+      <button className="addBtn">Add</button>
+    </div>
     </>
-    )
-}
+  );
+};
