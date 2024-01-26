8776f1d6a7d7808ceb827dc55b054e60ee8488aa
RikkeKristiansen98 <rikke.kristiansen@hotmail.com>
2024-01-26 21:49:55 +0100

    Some styling and <li> /ul in todolist

--- src/components/TodoForm.tsx
+++ src/components/TodoForm.tsx
@@ -1,36 +1,34 @@
+// TodoForm.tsx
 import React, { Dispatch, SetStateAction, useState } from "react";
 import { TodoService } from "../models/TodoService";
 import { TodoTypes } from "../models/TodoTypes";
 
-
 interface PropTypes {
-    setTodos : Dispatch<SetStateAction<TodoTypes[]>>
- }
- 
- 
- export const TodoForm:React.FC<PropTypes> = ({setTodos}) => {
- 
- 
-    const [newTodoText, setNewTodoText] = useState<string>("");
- 
- 
-    const handleAddTodo = () => {
-        if (newTodoText.trim() !== ""){
-            const newTodo = TodoService.addTodos
-            (newTodoText);
-            setTodos((prevTodo) => [...prevTodo,
-                newTodo]);
-            setNewTodoText("");
-        }
+  setTodos: Dispatch<SetStateAction<TodoTypes[]>>;
+}
+
+export const TodoForm: React.FC<PropTypes> = ({ setTodos }) => {
+  const [newTodoText, setNewTodoText] = useState<string>("");
+
+  const handleAddTodo = () => {
+    if (newTodoText.trim() !== "") {
+      const newTodo = TodoService.addTodos(newTodoText);
+      setTodos((prevTodo) => [...prevTodo, newTodo]);
+      setNewTodoText("");
     }
-    return (
-      
-        <div className="inputForm">
- <input type="text" value={newTodoText}
- onChange={(e) => setNewTodoText(e.target.value)}
- autoFocus={true}
- placeholder="Add a new task"/>
- <button onClick={handleAddTodo}>Add</button>
-        </div>
-    )
- }
+  };
+
+  return (
+    <div className="inputForm">
+      <input
+        type="text"
+        value={newTodoText}
+        onChange={(e) => setNewTodoText(e.target.value)}
+        autoFocus={true}
+        placeholder="Add a new task"
+      />
+      <button onClick={handleAddTodo}>Add</button>
+      <h1>To do list</h1>
+    </div>
+  );
+};
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,88 +1,83 @@
+// TodoList.tsx
 import { useState } from "react";
 import { TodoTypes } from "../models/TodoTypes";
 import { TodoService } from "../models/TodoService";
 import { TodoForm } from "./TodoForm";
 
-
 export const TodoList = () => {
- 
   const [todos, setTodos] = useState<TodoTypes[]>(TodoService.getTodos());
-  const [editingTodoId, setEditedTodoId] = useState<number | null> (null);
-  const [editedTodoText, setEditedTodoText] = useState<string>("")
- 
-  //functio for handling edit actions
+  const [editingTodoId, setEditedTodoId] = useState<number | null>(null);
+  const [editedTodoText, setEditedTodoText] = useState<string>("");
 
-
-  const handleEditStart = (id:number, text:string) =>{
-      setEditedTodoId(id);
-      setEditedTodoText(text);
-  }
-
-
-  const handleEditCancel =() => {
-      setEditedTodoId(null);
-      setEditedTodoText("")
-  }
-
-
-  const handleEditSave =(id:number) => {
-      if(editedTodoText.trim() !== ""){
-          const updateTodo = TodoService.updateTodo({
-              id, text:editedTodoText, completed:false
-          });
-setTodos((prevTodos) => prevTodos.map((todo) => (todo.id === id ? updateTodo : todo))
-);
-setEditedTodoId(null);
-setEditedTodoText("");
-  }
+  const handleEditStart = (id: number, text: string) => {
+    setEditedTodoId(id);
+    setEditedTodoText(text);
   };
 
-  const handleDeleteTodo = (id:number) => {
-      TodoService.deleteTodo(id);
-      setTodos((prevTodo) => prevTodo.filter((todo) => todo.id !== id))
-  }
+  const handleEditCancel = () => {
+    setEditedTodoId(null);
+    setEditedTodoText("");
+  };
+
+  const handleEditSave = (id: number) => {
+    if (editedTodoText.trim() !== "") {
+      const updateTodo = TodoService.updateTodo({
+        id,
+        text: editedTodoText,
+        completed: false,
+      });
+      setTodos((prevTodos) =>
+        prevTodos.map((todo) => (todo.id === id ? updateTodo : todo))
+      );
+      setEditedTodoId(null);
+      setEditedTodoText("");
+    }
+  };
+
+  const handleDeleteTodo = (id: number) => {
+    TodoService.deleteTodo(id);
+    setTodos((prevTodo) => prevTodo.filter((todo) => todo.id !== id));
+  };
+
   return (
-  <div className="todoContainer">
-   <div>
-<TodoForm setTodos = {setTodos}/>        
- </div>
- <div className="todos">
-
-
-{todos.map((todo) => (
-<div className="items" key={todo.id}>
-{editingTodoId == todo.id ? (
-<div className="editText">
-     
- <input type="text"
-      value={editedTodoText}
-      onChange={(e) => setEditedTodoText
-          (e.target.value)}
-          autoFocus={true} />
-          <button className="saveBtn" 
-          onClick={() => handleEditSave(todo.id)}>
-           Save
-          </button>
-          <button className="cancelBtn" 
-          onClick={() => handleEditCancel()}>
-          Cancel</button>
-  </div>
-): (
-  <div className="editBtn">
-<span>{todo.text}</span>
-<button 
-onClick={() => handleEditStart(todo.id, todo.text)}>
-Edit todo
-</button>
-</div>
-)}
-<button 
-onClick={() => handleDeleteTodo(todo.id)}>
-  Delete
-</button>
-</div>
-))}
-</div>
-</div>
-)};
-
+    <div className="main-container">
+      <div>
+        <TodoForm setTodos={setTodos} />
+      </div>
+      <div className="todos">
+        <ul>
+          {todos.map((todo) => (
+            <li key={todo.id} className="items">
+              {editingTodoId === todo.id ? (
+                <div className="editText">
+                  <input
+                    type="text"
+                    value={editedTodoText}
+                    onChange={(e) => setEditedTodoText(e.target.value)}
+                    autoFocus={true}
+                  />
+                  <button className="save-Btn" onClick={() => handleEditSave(todo.id)}>
+                    Save
+                  </button>
+                  <button className="cancel-Btn" onClick={() => handleEditCancel()}>
+                    Cancel
+                  </button>
+                </div>
+              ) : (
+                <div>
+                  <span>{todo.text}</span>
+                  <button className="edit-Btn" onClick={() => handleEditStart(todo.id, todo.text)}>
+                    Edit
+                  </button>
+                </div>
+              )}
+              <button className="delete-Btn" onClick={() => handleDeleteTodo(todo.id)}>
+                Delete
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
